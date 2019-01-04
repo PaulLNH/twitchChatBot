7ef5f13ebe06db33724f37010b2800ebaa5a53ed
plaird730 <plaird730@gmail.com>
2019-01-04 14:43:04 -0500

    Updated bot to have chat interactivity, fixed a bug where bot would spam the chat (would read it's own chat messages), added command list and basic commands

--- app.js
+++ app.js
@@ -1,35 +1,70 @@
-const tmi = require('tmi.js');
-require('dotenv').config();
+const tmi = require("tmi.js");
+require("dotenv").config();
+const triviaQuestions = require("./trivia");
 
 const options = {
-    options: {
-        debug: true
-    },
-    connection: {
-        reconnect: true
-    },
-    identity: {
-        username: process.env.TWITCH_USERNAME,
-        password: process.env.TWITCH_OAUTH
-    },
-    channels: ['draaxx']
-}
+  options: {
+    debug: true
+  },
+  connection: {
+    reconnect: true
+  },
+  identity: {
+    username: process.env.TWITCH_USERNAME,
+    password: process.env.TWITCH_OAUTH
+  },
+  channels: ["draaxx"]
+};
 
 const client = new tmi.client(options);
 client.connect();
 
-client.on('connected', (address, port) => {
-    client.action('draaxx', 'Yoooo! DraaxxBot here, holla at ya computer!');
+function displayQuestion() {
+  let question = ``;
+  question += `${triviaQuestions.trivia[0].question} ${
+    triviaQuestions.trivia[0].correct_answer
+  }`;
+  for (let i = 0; i < triviaQuestions.trivia[0].incorrect_answers.length; i++) {
+    console.log(triviaQuestions.trivia[0].incorrect_answers[i]);
+    question += `${triviaQuestions.trivia[0].incorrect_answers[i]}`;
+  }
+  return question;
+}
+
+client.on("connected", (address, port) => {
+  client.action(
+    "draaxx",
+    "here, type \'!commands\' to see what I can do!"
+  );
+  client
+    .say("draaxx", "/color GoldenRod")
+    .then(() => client.say("draaxx", displayQuestion()))
+    .catch(error => console.log(`The following error occured: ${error}`));
 });
 
-client.on('chat', (channel, user, message, self) => {
-    console.log('message:');
-    console.log(message);
-    console.log('user:');
-    console.log(user);
-    console.log('self:');
-    console.log(self);
-    client.action('draaxx', `Yooo @${user['display-name']}! What you doin?`);
+client.on("chat", (channel, user, message, self) => {
+  let messageParsed = message.toLowerCase().trim();
+  if (!self && messageParsed[0] === "!") {
+    console.log(messageParsed);
+    switch (messageParsed) {
+        case "!commands":
+        client.say("draaxx", `!hello - send salutations to yourself`)
+            .then(() => client.say("draaxx", `!trivia - start a new trivia game`))
+            .then(() => client.say("draaxx", `!bye - have bot wish you farewell`));
+        break;
+      case "!hello":
+        client.say("draaxx", `Yooo @${user["display-name"]}! What you doin?`);
+        break;
+      case "!trivia":
+        client.say("draaxx", `Trivia game coming soon!`);
+        break;
+        case "!bye":
+        client.say("draaxx", `Catch you on the flipside @${user["display-name"]}!`);
+        break;
+      default:
+        break;
+    }
+  }
 });
 
 // message = exact message player inputs as a string
